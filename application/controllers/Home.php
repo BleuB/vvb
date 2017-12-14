9f7e6785c4149c9189087eb1540ba0e42afce867
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-12-14 17:12:39 +0100

    debut controller home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,21 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	private $dataH = array();//data head
+	private $dataHd = array();//data header
+	private $dataC = array();//data corp
+	private $dataF = array();//data footer
+
+	public function __construct()
+    {
+        parent::__construct();
+        //chargement models
+		
+    }
 	public function index()
 	{
 		$this->load->view('welcome_message');
