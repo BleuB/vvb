2487d05f3d100f9b4281db2574712f969e4ea180
SullivanAnge <ange.sullivan@hotmail.fr>
2017-12-14 17:51:13 +0100

    ajout scripts

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -5,4 +5,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 	</footer>
+	<!-- script -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo js_url('js'); ?>"></script>
+</script>
 </body>
+</html>
