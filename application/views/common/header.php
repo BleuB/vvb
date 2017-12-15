8a1bdb605acabbf9cdb8ca283783343cff3607fa
SullivanAnge <ange.sullivan@hotmail.fr>
2017-12-15 11:13:21 +0100

    header nav

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -3,5 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <body>
 	<header>
+		<div class="container-fluid">
+			
+		
+			<nav class="navbar navbar-fixed-top">
+	  			<div class="container-fluid">
+	   
+	      		<ul class="nav navbar-nav">
+	        		<li id="first"><a href="#">Annonces</a></li>
+	        		<li><a href="#">Conseil</a></li>
+	        		<li><a href="#"><i class="fa fa-user-o" aria-hidden="true"></i> Mon Espace</a></li>
+	        		<li id="passAnn"><a href="#">Passer une annonce</a></li>
+	        
+	      		</ul>
+	      
+	     
+	    
+	  			</div>
+			</nav>
+			<div id="head-rech">
+				<p>
+				hello world
+				</p>
+			</div>
+		</div>
 		
 	</header>
