4d73ab70e6e7e77f098a315f2c166536ccd3a32d
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-12-15 11:53:42 +0100

    up dredi

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -8,7 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<!-- script -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php echo js_url('js'); ?>"></script>
+<script type="text/javascript" src="<?php echo js_url('admin_js'); ?>"></script>
 </script>
 </body>
 </html>
--- application/views/admin/head.php
+++ application/views/admin/head.php
@@ -13,6 +13,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 		<!-- CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo css_url('style');?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo css_url('admin_style');?>" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
