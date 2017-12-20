67c2fe5c2305d2b0072292936a66d5e7c0071c50
SullivanAnge <ange.sullivan@hotmail.fr>
2017-12-20 12:15:38 +0100

    conseils

--- application/views/conseils/indexe_conseils.php
+++ application/views/conseils/indexe_conseils.php
@@ -1,15 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<div>
+
 
 	<div class="row">
-		<h3 id="conseils" class="col-md-12">Nos Conseils</h3>
+		<h3 id="conseils" class="col-md-3 col-md-offset-4">Nos Conseils</h3>
 	</div>
-	<!--<div class="row">
-		<div class="col-md-6 col-md-offset-1">
-			<strong>Avant de louer, nos conseils</strong>
-			<p>
+	
+<div class="row">
+	<div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+			<div class="col-xs-12 col-md-6 text-justify vertical-align-item">
+				<strong>Avant de louer, nos conseils</strong>
+				<p>
 					<i class="fa fa-square" aria-hidden="true"></i> Avant de réserver un appartement, un gîte une maison ou une chambre d’hôtel, trouvé par le biais des annonces publiées dans cet ouvrage, nous vous recommandons de lire nos conseils. Vous éviterez ainsi toute déception lors de votre arrivée et serez à même de vous défendre en cas de problèmes en cours de location.
 				</p>
 				<p>
@@ -37,22 +39,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					</i>
 					<a href="<?php echo site_url('Conseil/recours');?>"> Recours</a>
 				</p>
+			</div><!--
+	 -->	<div class=" hidden-xs col-md-6 col-md-offset-0 vertical-align-item">
+				<img class="img-responsive" src=" <?php echo img_url('conseils/person-woman-apple-hotel.jpg'); ?>">
+			</div>
 		</div>
-	</div>-->
-
-	<div class="row">
-		<div class="col-md-4 col-md-offset-1">
-			<strong>Avant de louer, nos conseils</strong>
-			<p>
-			 	Avant de réserver un appartement, un gîte une maison ou une chambre d’hôtel, trouvé par le biais des annonces publiées dans cet ouvrage, nous vous recommandons de lire nos conseils. Vous éviterez ainsi toute déception lors de votre arrivée et serez à même de vous défendre en cas de problèmes en cours de location.
-			</p>
-		</div>
-		<div class="col-md-4">
-			<img class="img-responsive" src="<?php echo img_url("conseils/precaution200x200.jpg"); ?>">
-		</div>
-	</div>
-
 </div>
+
 <div id="blockvide" class="hidden-xs">
 	
 </div>
--- application/views/conseils/precautions.php
+++ application/views/conseils/precautions.php
@@ -1,23 +1,23 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<section id="content" class="row">
-	<section id="conseils" itemprop="text">
+		<h3 id="conseils" class="col-md-3 col-md-offset-4">Précautions</h3>
+	<div class="row">
 		<div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
-			<h3>Précautions</h3>
+			
 			<strong>Quelles précautions prendre avant d’envoyer son chèque de réservation ?</strong>
 			<br>
 			<div class="row">
 				<p class="col-xs-12 col-md-6 text-justify vertical-align-item">
 					<i class="fa fa-square" aria-hidden="true"></i> La description des logements qui vous sont proposés par le biais d’annonces ne peut être, par manque de place, très précise et exhaustive. En règle générale, vous ne pouvez, avant de donner votre accord, vous déplacer pour visiter le logement. La réservation va donc se faire par correspondance et bien souvent suite à un simple appel téléphonique. Pour éviter d’être déçu quand vous prendrez connaissance des lieux loués, vous devez avant d’envoyer votre chèque de réservation ou tout engagement écrit, obtenir un certain nombre d’informations Que la location soit proposée directement par un propriétaire, par une agence immobilière, par un responsable de gîte rural ou par un hôtelier, les démarches suivantes sont à effectuer.
-				</p><!--
-		 --><div class="col-xs-8 col-xs-offset-2 col-md-6 col-md-offset-0 vertical-align-item">
-					<?php //echo my_img('conseils/precaution.jpg','','img-responsive center-block'); ?>
+				</p>
+		 		<div class="col-xs-8 col-xs-offset-2 col-md-6 col-md-offset-0 vertical-align-item">
+					<img class="img-responsive" src="<?php echo img_url('conseils/precaution300x300.jpg'); ?>">
 				</div>
 			</div>
 		</div>
-	</section>
-</section>
-<div id="blockvide" class="hidden-xs">
+	</div>
+
+<div id="blockvide2" class="hidden-xs">
 	
 </div>
