f587bbab27b3ba0a1623181397531291871d87fb
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-12-20 11:37:56 +0100

    ajout get pub function

--- application/models/Pub_model.php
+++ application/models/Pub_model.php
@@ -16,4 +16,15 @@ class Pub_model extends CI_Model
         $this->db->where('id', $id);
         $this->db->update('pubs', $data);
     }
+    public function get_pub_list($emplacement,$limit){
+        $row = $this->db->select()
+                    ->from('pubs')
+                    ->where('emplacement', $emplacement)
+                    ->where('active', 1)
+                    ->limit($limit)
+                    ->order_by('title', 'RANDOM')
+                    ->get()
+                    ->result_array();
+        return $row;
+    }
 }
