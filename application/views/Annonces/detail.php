0a5ed421256c7f11052e261ae72a7379353eda73
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2018-01-15 17:41:29 +0100

    changement page detail

--- application/views/Annonces/detail.php
+++ application/views/Annonces/detail.php
@@ -1,6 +1,9 @@
-<?php //var_dump($images) ?>
-<div class="row">
-	<div class="col-xs-12"> <!-- detail annonce -->
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
       <section id="detail_annonce">
         <div class="row">  
           <h2 class="col-md-12"><?php echo $annonce['title']; ?></h2>
@@ -10,103 +13,197 @@
               <h4 class="col-md-12"><?php echo $annonce['town']; ?></h4>
         </div>
         </section>
-	</div>
-</div>
-<div class="row"> <!-- slider + infos + tarifs -->
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="row">
+        <div class="col-md-12">
+          <!-- Slider -->
+        
+            <div class="col-sm-3" id="slider-thumbs">
+                <!-- Bottom switcher of slider -->
+                <ul class="hide-bullets">
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-0">
+                            <img src="http://placehold.it/150x150&text=zero">
+                        </a>
+                    </li>
 
-         <div class="col-md-4"> <!-- slider -->
-            <div  class="carousel slide" data-ride="carousel">
-              <!-- Indicators -->
-              <ol class="carousel-indicators">
-                <li data-target="#slider" data-slide-to="0" class="active"></li>
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-1"><img src="http://placehold.it/150x150&text=1"></a>
+                    </li>
 
-                <?php $nbSlides = count($images); ?>
-                <?php for($i = 1 ; $i < $nbSlides ; $i++): ?>
-                <li data-target="#slider" data-slide-to="<?php echo $i; ?>"></li>
-                <?php endfor; ?>
-              </ol>
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-2"><img src="http://placehold.it/150x150&text=2"></a>
+                    </li>
 
-              <!-- Wrapper for slides -->
-              <div class="carousel-inner" role="listbox">
-                <?php foreach ($images as $img): ?>
-                  
-                <?php if ($img == $images[0]): // Si 1ere images ?>
-                <div class="item active">
-                  <img src="<?php echo img_url("annonces/".$img['file']);?>" alt="<?php echo $annonce['title']; ?>">
-                  <div class="carousel-caption">
-                    <!-- legende -->
-                  </div>
-                </div>
-                <?php else: // Sinon ?>
-                <div class="item">
-                  <img src="<?php echo img_url("annonces/".$img['file']);?>" alt="<?php echo $annonce['title']; ?>">
-                  <div class="carousel-caption">
-                    <!-- legende -->
-                  </div>
-                </div>
-                <?php endif; ?>
-                <?php endforeach; ?>
-              </div>
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-3"><img src="http://placehold.it/150x150&text=3"></a>
+                    </li>
 
-              <!-- Controls -->
-              <a class="left carousel-control" href="#slider" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="right carousel-control" href="#slider" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-4"><img src="http://placehold.it/150x150&text=4"></a>
+                    </li>
+
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-5"><img src="http://placehold.it/150x150&text=5"></a>
+                    </li>
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-6"><img src="http://placehold.it/150x150&text=6"></a>
+                    </li>
+
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-7"><img src="http://placehold.it/150x150&text=7"></a>
+                    </li>
+
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-8"><img src="http://placehold.it/150x150&text=8"></a>
+                    </li>
+
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-9"><img src="http://placehold.it/150x150&text=9"></a>
+                    </li>
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-10"><img src="http://placehold.it/150x150&text=10"></a>
+                    </li>
+
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-11"><img src="http://placehold.it/150x150&text=11"></a>
+                    </li>
+
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-12"><img src="http://placehold.it/150x150&text=12"></a>
+                    </li>
+
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-13"><img src="http://placehold.it/150x150&text=13"></a>
+                    </li>
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-14"><img src="http://placehold.it/150x150&text=14"></a>
+                    </li>
+
+                    <li class="col-sm-12">
+                        <a class="thumbnail" id="carousel-selector-15"><img src="http://placehold.it/150x150&text=15"></a>
+                    </li>
+                </ul>
             </div>
-          </div> <!-- /slider -->
+            <div class="col-sm-8">
+                <div class="col-xs-12" id="slider">
+                    <!-- Top part of the slider -->
+                    <div class="row">
+                        <div class="col-sm-12" id="carousel-bounding-box">
+                            <div class="carousel slide" id="myCarousel">
+                                <!-- Carousel items -->
+                                <div class="carousel-inner">
+                                    <div class="active item" data-slide-number="0">
+                                        <img src="http://placehold.it/470x480&text=zero"></div>
 
-          <div  class="col-md-4"> <!-- infos -->
-            
+                                    <div class="item" data-slide-number="1">
+                                        <img src="http://placehold.it/470x480&text=1"></div>
+
+                                    <div class="item" data-slide-number="2">
+                                        <img src="http://placehold.it/470x480&text=2"></div>
+
+                                    <div class="item" data-slide-number="3">
+                                        <img src="http://placehold.it/470x480&text=3"></div>
+
+                                    <div class="item" data-slide-number="4">
+                                        <img src="http://placehold.it/470x480&text=4"></div>
+
+                                    <div class="item" data-slide-number="5">
+                                        <img src="http://placehold.it/470x480&text=5"></div>
+                                    
+                                    <div class="item" data-slide-number="6">
+                                        <img src="http://placehold.it/470x480&text=6"></div>
+                                    
+                                    <div class="item" data-slide-number="7">
+                                        <img src="http://placehold.it/470x480&text=7"></div>
+                                    
+                                    <div class="item" data-slide-number="8">
+                                        <img src="http://placehold.it/470x480&text=8"></div>
+                                    
+                                    <div class="item" data-slide-number="9">
+                                        <img src="http://placehold.it/470x480&text=9"></div>
+                                    
+                                    <div class="item" data-slide-number="10">
+                                        <img src="http://placehold.it/470x480&text=10"></div>
+                                    
+                                    <div class="item" data-slide-number="11">
+                                        <img src="http://placehold.it/470x480&text=11"></div>
+                                    
+                                    <div class="item" data-slide-number="12">
+                                        <img src="http://placehold.it/470x480&text=12"></div>
+
+                                    <div class="item" data-slide-number="13">
+                                        <img src="http://placehold.it/470x480&text=13"></div>
+
+                                    <div class="item" data-slide-number="14">
+                                        <img src="http://placehold.it/470x480&text=14"></div>
+
+                                    <div class="item" data-slide-number="15">
+                                        <img src="http://placehold.it/470x480&text=15"></div>
+                                </div>
+                                <!-- Carousel nav -->
+                                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                </a>
+                                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/Slider-->
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="mid-title">Localisation</h3>
+            <div class="thumbnail">
+              <div class="map_responsive">
+                <iframe width="1000" height="450" frameborder="0" style="border:0;max-width: 100%;" src="https://www.google.com/maps/embed/v1/place?q=<?php echo str_replace(" ", "+ ", $annonce['adress'] . " " . $annonce['cp'] . "
+                  " . $annonce['town']); ?>&key=AIzaSyCVJ3Q1FZbMOBBdpgXHElbvNIk4t5myAkI&zoom=9" allowfullscreen></iframe>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="mid-title">Description</h3>
+          <p class="text-justify">
+            <?php echo $annonce['descr_longue']; ?>
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
             <h3 class="mid-title">Informations</h3>
             <div class="row">
-            	<div class="col-md-6">
-	              <ul class="list-unstyled">
-	                <li>Capacité : <strong><?php echo $annonce['capacite']; ?></strong></li>
-	                <li>Durée minimun séjour : <strong><?php echo $annonce['duree_sejour']; ?></strong></li>
-	                <li>Couchages : <strong><?php echo $annonce['couchage']; ?></strong></li>
-	              </ul>
-            	</div>
-	            <div class="col-md-6">
-	              <ul class="list-unstyled">
-	                <li>Type de logement : <strong><?php echo $annonce_type[$annonce['type']-1]['type'];?></strong></li>
-	                <li>Chambres / Emplacements : <strong><?php echo $annonce['chambre']; ?></strong></li>
-	                <li>Surface : <strong><?php echo $annonce['surface']; ?></strong></li>
-	              </ul>
-	            </div>
-			       </div>
-
-          </div>
-          <div class="col-xs-12 col-md-4">
-            <h3 class="mid-title">Description</h3>
-            <p class="text-justify">
-              <?php echo $annonce['descr_longue']; ?>
-            </p>
-          </div>
+              <div class="col-md-6">
+                <ul class="list-unstyled">
+                  <li>Capacité : <strong><?php echo $annonce['capacite']; ?></strong></li>
+                  <li>Durée minimun séjour : <strong><?php echo $annonce['duree_sejour']; ?></strong></li>
+                  <li>Couchages : <strong><?php echo $annonce['couchage']; ?></strong></li>
+                </ul>
+              </div>
+              <div class="col-md-6">
+                <ul class="list-unstyled">
+                  <li>Type de logement : <strong><?php echo $annonce_type[$annonce['type']-1]['type'];?></strong></li>
+                  <li>Chambres / Emplacements : <strong><?php echo $annonce['chambre']; ?></strong></li>
+                  <li>Surface : <strong><?php echo $annonce['surface']; ?></strong></li>
+                </ul>
+              </div>
+             </div>
         </div>
-
-          
-
-  
-  <div class="row">
-          <div class="hidden-xs col-md-4 " >
-            
-                
-                <?//php $nbSlides = count($images); ?>
-                <?php for($i = 0 ; $i < $nbSlides ; $i++): ?>
-                  <?php if($i==0||$i==5){echo '<div class="row">';}?>
-                  
-                <img class="col-md-2 img-slide" data-target="#slider" data-slide-to="<?php echo $i; ?>" src="<?php echo img_url("annonces/".$images[$i]['file']);?>">
-                <?php if($i==4||$i==10||$i==$nbSlides-1){echo '</div>';}?>
-                <?php endfor; ?>        
-          
-          </div>
-          <div class="col-xs-12 col-md-7"> <!-- tarifs -->
-            <h3 class="mid-title">À partir de</h3>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <h3 class="mid-title">À partir de</h3>
             <div class="row">
               <div class="col-xs-6 col-sm-6 col-md-6  text-center-on-sm">
                 <h4>Hors Saison</h4>
@@ -131,25 +228,9 @@
                 </p>
               </div>
             </div>
-          </div>
-     </div>
-
-     <div class="row"> <!-- desc + maps + contact -->
-
-          
-
-          <div class="col-xs-12 col-md-4"> <!-- maps -->
-            <h3 class="mid-title">Localisation</h3>
-            <div class="thumbnail">
-              <div class="map_responsive">
-                <iframe width="600" height="450" frameborder="0" style="border:0;max-width: 100%;" src="https://www.google.com/maps/embed/v1/place?q=<?php echo str_replace(" ", "+ ", $annonce['adress'] . " " . $annonce['cp'] . "
-                  " . $annonce['town']); ?>&key=AIzaSyCVJ3Q1FZbMOBBdpgXHElbvNIk4t5myAkI&zoom=9" allowfullscreen></iframe>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xs-12 col-md-4"> <!-- contact -->
-            <h3 class="mid-title">Contacter le propriétaire</h3>
+        </div>
+        <div class="col-md-6">
+          <h3 class="mid-title">Contacter le propriétaire</h3>
             <table class="table table-condensed">
               <tr>
                 <td><i class="fa fa-user" aria-hidden="true" title="Fixe"></i></td>
@@ -194,6 +275,8 @@
                 </tr>
                 <?php } ?>
             </table>
-          </div>
-
         </div>
+      </div>
+    </div>
+  </div>
+</div>
